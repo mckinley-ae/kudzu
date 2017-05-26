8c2960f26489728863daa713da05532c5286e2ae
Alec McKinley <mckinley.ae@gmail.com>
2017-05-25 21:20:18 -0400

    added local and production settings

--- ats/tests.py
+++ ats/tests.py
@@ -5,7 +5,7 @@ from .models import *
 class ATS_views_test_case(TestCase):
 
     def test_index(self):
-            resp = self.client.get('')
+            resp = self.client.get('') #index url
             self.assertEqual(resp.status_code, 200)
             self.assertTrue('latest_candidate_list' in resp.context)
             self.assertEqual([candidate.pk for candidate in resp.context['latest_candidate_list']], [1])
---
+++ mysite/settings/local.py
@@ -0,0 +1,22 @@
+#import os
+from mysite.settings.base_settings import *
+
+SECRET_KEY = '=mr&_u3u6wtdfq)=6)&jn!gq3wd-q6)cl$(#chfrwo4bz^wzb1'
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+#ALLOWED_HOSTS = ['104.131.184.18', 'www.kudzu.cc', 'kudzu.cc']
+
+# Application definition
+
+INSTALLED_APPS = [
+	'nltk',
+	'PyPDF2',
+	'datetimewidget', #https://github.com/asaglimbeni/django-datetime-widget
+	'ats.apps.AtsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+]
